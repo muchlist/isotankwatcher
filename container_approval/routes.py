9066532f7c19dcd7c15b42bdc4b4d2533c860ea7
mrtsdc <whois.muchlis@gmail.com>
2020-04-12 18:44:08 +0800

    set bagendang Bongkar-Delivery tidak ada gateout

--- container_approval/routes.py
+++ container_approval/routes.py
@@ -44,7 +44,6 @@ def check_to_ready_doc(check_id):
         "doc_level": 1,
         "container.branch": claims["branch"][0]
     }
-    print(query)
     update = {
         '$set': {"doc_level": 2,
                  "approval.checked_by": get_jwt_identity(),
@@ -148,25 +147,43 @@ def approval_foreman_doc(check_id):
     if container_check is None:
         return {"message": "Gagal update. Dokumen ini telah di ubah oleh seseorang sebelumnya. Harap cek data terbaru!"}, 402
     else:
-        #CEK APAKAH STATUS TIDAK SAMA DENGAN NIHIL
+        # CEK APAKAH STATUS TIDAK SAMA DENGAN NIHIL
         dammaged = container_check["status"] != "NIHIL"
-        lvl_up_container_info_lvl(container_check["container_id"], dammaged)
+        # CEK APAKAH PAKSA Container Info KE LVL 9, "BONGKAR-DELIVERY  - Step three"
+        forced = forced_to_finish(
+            container_check["container"]["activity"], container_check["position_step"])
+        container_info = lvl_up_container_info_lvl(
+            container_check["container_id"], dammaged, forced)
 
-    # if container_check["position_step"] == "four":
     #     # TODO BIKIN PDF
 
     return jsonify(container_check), 201
 
 
-def lvl_up_container_info_lvl(container_id, dammaged):
+def lvl_up_container_info_lvl(container_id, dammaged, forced_lvl):
     query = {'_id': ObjectId(container_id)}
     update = {'$inc': {"document_level": 1}}
-    
+    set_embed = {}
+    if forced_lvl:
+        update = {} #jika force maka hilangakan increment
+        set_embed["document_level"] = 9  # tindih dengan naik langsung ke lvl 9
     if dammaged:
-        update["dammaged"] = True
+        set_embed["dammaged"] = True
+    if set_embed != {}:
+        update['$set'] = set_embed
 
     try:
-        mongo.db.container_info.find_one_and_update(
-            query, update, return_document=False)
+        container_info = mongo.db.container_info.find_one_and_update(
+            query, update, return_document=True)
     except:
         return {"message": "galat menaikkan lvl pada container_info"}, 500
+
+    return container_info
+
+
+def forced_to_finish(activity, check_step):
+    """
+    Untuk cabang yang tidak memiliki gate out saat aktifitas bongkar delivery
+    maka ketika di step 3 akan langsung mengupdate container info ke lvl 9 (finish)
+    """
+    return activity == "BONGKAR-DELIVERY" and check_step == "three"
